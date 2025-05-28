680f15e89233c25aaf618a3d183066ae0ee288d4
Kristofer Medeiros <kristoferavka@gmail.com>
2025-05-28 15:44:30 -0400

    Fixed Tailwind, extension now displays

---
+++ src/popup/main.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import '../styles/global.css'
+import Popup from './popup'
+
+createRoot(document.getElementById('root')!).render(<Popup />)
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./public/popup.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        vt323: ["VT323", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+}
