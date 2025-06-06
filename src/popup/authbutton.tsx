975fce1b99278cfc11bb18f6b71f168c0a840bf5
Kristofer Medeiros <kristoferavka@gmail.com>
2025-06-05 21:56:17 -0400

    Changed from popup to a sidebar in chrome

---
+++ src/popup/authbutton.tsx
@@ -0,0 +1,7 @@
+export default function authbutton(){
+    //Visual display
+    
+
+
+    //Nitty gritty
+}
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -2,7 +2,7 @@ import CalighterIcon from '/Calighter_icon_48x48.png';
 
 export default function Popup() {
     return (
-        <div className="w-96 h-[468px] bg-white rounded-[10px] flex items-start justify-center pt-8 gap-2">
+        <div className="w-full min-h-screen bg-white flex items-start justify-center pt-8 gap-2">
             <img className="w-12 h-12" src={CalighterIcon} alt="Calighter Icon"/>
             <h1 className="text-black text-5xl font-normal font-['VT323']">Calighter</h1>
         </div>
