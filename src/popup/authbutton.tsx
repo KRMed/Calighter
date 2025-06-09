a7df0cea2fa77d1362523e5099fc1bfc6e777045
Kristofer Medeiros <kristoferavka@gmail.com>
2025-06-08 22:00:43 -0400

    Changed icon to open side panel on click, added sign in button

--- src/popup/authbutton.tsx
+++ src/popup/authbutton.tsx
@@ -1,7 +1,19 @@
-export default function authbutton(){
-    //Visual display
-    
+"use client";
+import GCal from "/Google_Calendar_icon_(2020).svg.png"
+
+export default function AuthButton(){
+    return(
+        //Visual display
+        <div>
+            <button className="hover:drop-shadow-md hover:bg-gray-100 transition-colors duration-200 justify-center items-center flex gap-2 bg-white border-2 rounded-full p-4 w-full max-w-md">
+                <img className="w-9 h-9" src={GCal} alt="Google Calendar Icon"/>
+                <h2 className="text-black font-medium">Sign in with Google</h2>
+            </button>
+
+        </div>
 
 
-    //Nitty gritty
+
+        //Nitty gritty
+    )
 }
