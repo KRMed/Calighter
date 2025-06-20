b2769f8079355182d1e5e0440e7e9532e037fc09
Kristofer Medeiros <kristoferavka@gmail.com>
2025-06-20 13:56:48 +0800

    Added simple google auth features and chrome functionality

--- src/popup/authbutton.tsx
+++ src/popup/authbutton.tsx
@@ -1,19 +1,16 @@
 "use client";
 import GCal from "/Google_Calendar_icon_(2020).svg.png"
+import { OAuth } from "./oauth";
+// Using named export instead of default export in case of multiple exports and seems to be industry standard
 
-export default function AuthButton(){
+export function AuthButton(){
     return(
         //Visual display
         <div>
-            <button className="hover:drop-shadow-md hover:bg-gray-100 transition-colors duration-200 justify-center items-center flex gap-2 bg-white border-2 rounded-full p-4 w-full max-w-md">
+            <button onClick={OAuth} className="hover:drop-shadow-md hover:bg-gray-100 transition-colors duration-200 justify-center items-center flex gap-2 bg-white border-2 rounded-full p-4 w-full max-w-md">
                 <img className="w-9 h-9" src={GCal} alt="Google Calendar Icon"/>
                 <h2 className="text-black font-medium">Sign in with Google</h2>
             </button>
-
         </div>
-
-
-
-        //Nitty gritty
     )
 }
