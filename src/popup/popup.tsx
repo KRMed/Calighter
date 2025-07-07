b080b4986a330cff8f7e4573d1b225a1f9544249
Kristofer Medeiros <kristoferavka@gmail.com>
2025-07-07 17:04:36 +0800

    Added date parsing and scripting for highlighted text

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -4,15 +4,54 @@ import CalighterIcon from '/Calighter_icon_48x48.png'
 import { AuthButton } from './authbutton'
 import { isAuthenticated, terminateToken } from './oauth';
 import { handleAddEvent } from './api';
-import TextField from '@mui/material/TextField';
+import {Checkbox, FormControlLabel, TextField} from '@mui/material';
+import Switch from '@mui/material/Switch';
+import * as chrono from 'chrono-node';
 
 export default function Popup() {
     //Allows for usage of null, true, and false because typescript naturally does not allow for assignment of null
     const [authed, setAuthed] = useState<boolean | null>(null);
+    const [input, setInput] = useState<boolean>(false)
+    const [eventTitle, setEventTitle] = useState<string>("");
+    const [start, setStart] = useState<string>("");
+    const [end, setEnd] = useState<string>(""); 
 
     useEffect(() => {
         isAuthenticated().then(setAuthed);
     }, []);
+
+    useEffect(() => {
+        let previousText = "";
+        const interval = setInterval(() => {
+            if (!input) return;
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                if (!tabs[0]?.id) return;
+                chrome.tabs.sendMessage(
+                    tabs[0].id!,
+                    { action: "getSelectedText" },
+                    (response) => {
+                        if (chrome.runtime.lastError) {
+                            return;
+                        }
+                        if (response && response.selectedText && response.selectedText !== previousText) {
+                            previousText = response.selectedText;
+                            const parsedDate = chrono.parse(response.selectedText);
+                            console.log("Parsed Date:", parsedDate);
+                            if (parsedDate && parsedDate.length > 0) {
+                                const {start: chronoStart, end: chronoEnd} = parsedDate[0];
+                                setStart(chronoStart ? chronoStart.date().toLocaleString() : "");
+                                setEnd(chronoEnd ? chronoEnd.date().toLocaleString() : "");
+                            }
+
+                            setEventTitle(response.selectedText);
+                        }
+                    }
+                );
+            });
+        }, 500);
+
+        return () => clearInterval(interval);
+    }, [input]);
 
     if (authed === null) {
         return (
@@ -36,36 +75,51 @@ export default function Popup() {
 
             {/* If the user is authenticated, show text boxes*/}
                 {authed && (
-                    //When Nlp model is comlete text boxes should work for what we wanna do
                     <div>
+                        <div className='mb-4 flex-col justify-center gap-2 w-1/2 bg-gray-300 p-4 rounded mx-auto'>
+                            <div>
+                                <button className="bg-transparent hover:text-red-600" onClick={async () => {
+                                    await terminateToken();
+                                    setAuthed(false);
+                                    }}>Sign Out
+                                </button>
+                            </div>
+                            <FormControlLabel
+                                label="Dark Mode"
+                                labelPlacement="start"
+                                control={<Checkbox />}
+                            />
+                        </div>
+                        <div className="mb-8 flex items-center justify-center gap-2 w-full">
+                            <p className="text-black text-2xl font-normal font-['VT323']">Enable Highlight Input</p> 
+                            <Switch checked={input} onChange={(e) => setInput(e.target.checked)} />
+                        </div>
                         <div className="mb-4 flex items-center gap-2 w-full">
                             <p className='font-bold min-w-[70px]'>Title</p>
-                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 } }} id="filled-basic" label="" variant="filled" />
+                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 }, '& .MuiFilledInput-input': { fontSize: 14 } }} id="filled-basic" label="" variant="filled" value={eventTitle} onChange={(e) => setEventTitle(e.target.value)} />
                         </div>
                         <div className="mb-4 flex items-center gap-2 w-full">
                             <p className='font-bold min-w-[70px]'>Start</p>
-                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 } }} id="filled-basic" label="" variant="filled" />
+                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 }, '& .MuiFilledInput-input': { fontSize: 14 } }} id="filled-basic" label="" variant="filled" value={start} onChange={(e) => setStart(e.target.value)} />
                         </div>
                         <div className="mb-4 flex items-center gap-2 w-full">
                             <p className='font-bold min-w-[70px]'>End</p>
-                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 }, '& .MuiFilledInput-input': { fontSize: 14 } }} id="filled-basic" label="" variant="filled" />
+                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 }, '& .MuiFilledInput-input': { fontSize: 14 } }} id="filled-basic" label="" variant="filled" value={end} onChange={(e) => setEnd(e.target.value)} />
                         </div>
                         <div className="mb-4 flex items-center gap-2 w-full">
                             <p className='font-bold min-w-[70px]'>Location</p>
-                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 } }} id="filled-basic" label="" variant="filled" />
+                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 }, '& .MuiFilledInput-input': { fontSize: 14 } }} id="filled-basic" label="" variant="filled" />
                         </div>
                         <div className="mb-4 flex items-center gap-2 w-full">
                             <p className='font-bold min-w-[70px]'>Description</p>
                             <TextField id="outlined-multiline-flexible" size="small" sx={{ width: '80%' }} label="" maxRows={5} />
                             {/* Figure out whats wrong with the multiline text field */}
                         </div>
-                        <button onClick={handleAddEvent} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">
-                            Add Event
-                        </button>
-                        <button onClick={async () => {
-                            await terminateToken();
-                            setAuthed(false);
-                        }}>Sign Out</button>
+                        <div className="mb-8 mt-5 flex justify-center gap-2 w-full">
+                            <button onClick={handleAddEvent} className="text-black text-2xl font-normal font-['VT323'] outline outline-2 px-4 py-2 rounded" style={{ outlineColor: '#07BCFA' }}>
+                                Add to Calendar
+                            </button>
+                        </div>
                     </div>
                 )}
             </div>
