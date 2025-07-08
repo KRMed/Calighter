f26681a337a5163298c22f21c7713b099e1af004
Kristofer Medeiros <kristoferavka@gmail.com>
2025-07-08 14:31:14 +0800

    Added text trash feature and updated text boxes

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -4,8 +4,7 @@ import CalighterIcon from '/Calighter_icon_48x48.png'
 import { AuthButton } from './authbutton'
 import { isAuthenticated, terminateToken } from './oauth';
 import { handleAddEvent } from './api';
-import {Checkbox, FormControlLabel, TextField} from '@mui/material';
-import Switch from '@mui/material/Switch';
+import {TextField, Switch} from '@mui/material';
 import * as chrono from 'chrono-node';
 
 export default function Popup() {
@@ -15,6 +14,9 @@ export default function Popup() {
     const [eventTitle, setEventTitle] = useState<string>("");
     const [start, setStart] = useState<string>("");
     const [end, setEnd] = useState<string>(""); 
+    const [location, setLocation] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+    const [trash, setTrash] = useState<boolean>(false);
 
     useEffect(() => {
         isAuthenticated().then(setAuthed);
@@ -76,49 +78,47 @@ export default function Popup() {
             {/* If the user is authenticated, show text boxes*/}
                 {authed && (
                     <div>
-                        <div className='mb-4 flex-col justify-center gap-2 w-1/2 bg-gray-300 p-4 rounded mx-auto'>
-                            <div>
-                                <button className="bg-transparent hover:text-red-600" onClick={async () => {
-                                    await terminateToken();
-                                    setAuthed(false);
-                                    }}>Sign Out
-                                </button>
-                            </div>
-                            <FormControlLabel
-                                label="Dark Mode"
-                                labelPlacement="start"
-                                control={<Checkbox />}
-                            />
+                        <div className='mb-6 flex flex-col items-start gap-2 w-2/3 bg-gray-300 p-4 rounded mx-auto'>
+                            <label className='text-lg'>
+                                Dark Mode
+                                <input type="checkbox" className="ml-4 w-6 h-6" />
+                            </label>
+                            <button className="bg-transparent hover:text-red-600 text-lg" onClick={async () => {
+                                await terminateToken();
+                                setAuthed(false);
+                            }}>Sign Out
+                            </button>
+                            <button className='bg-transparent hover:text-red-600 text-lg'>
+                                Switch Accounts
+                            </button>
+
                         </div>
-                        <div className="mb-8 flex items-center justify-center gap-2 w-full">
+                        <div className="mb-4 flex items-center justify-center gap-2 w-full">
                             <p className="text-black text-2xl font-normal font-['VT323']">Enable Highlight Input</p> 
                             <Switch checked={input} onChange={(e) => setInput(e.target.checked)} />
                         </div>
                         <div className="mb-4 flex items-center gap-2 w-full">
-                            <p className='font-bold min-w-[70px]'>Title</p>
-                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 }, '& .MuiFilledInput-input': { fontSize: 14 } }} id="filled-basic" label="" variant="filled" value={eventTitle} onChange={(e) => setEventTitle(e.target.value)} />
+                            <TextField id="outlined-basic" fullWidth size="small" sx={{ '& .MuiFilledInput-input': { fontSize: 13 } }} placeholder='Event Title' value={eventTitle} onChange={(e) => setEventTitle(e.target.value)} />
                         </div>
                         <div className="mb-4 flex items-center gap-2 w-full">
-                            <p className='font-bold min-w-[70px]'>Start</p>
-                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 }, '& .MuiFilledInput-input': { fontSize: 14 } }} id="filled-basic" label="" variant="filled" value={start} onChange={(e) => setStart(e.target.value)} />
+                            <TextField id="outlined-basic" fullWidth size="small" sx={{ '& .MuiFilledInput-input': { fontSize: 13 } }} placeholder='Start Time' value={start} onChange={(e) => setStart(e.target.value)} />
                         </div>
                         <div className="mb-4 flex items-center gap-2 w-full">
-                            <p className='font-bold min-w-[70px]'>End</p>
-                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 }, '& .MuiFilledInput-input': { fontSize: 14 } }} id="filled-basic" label="" variant="filled" value={end} onChange={(e) => setEnd(e.target.value)} />
+                            <TextField id="outlined-basic" fullWidth size="small" sx={{ '& .MuiFilledInput-input': { fontSize: 13 } }} placeholder='End Time' value={end} onChange={(e) => setEnd(e.target.value)} />
                         </div>
                         <div className="mb-4 flex items-center gap-2 w-full">
-                            <p className='font-bold min-w-[70px]'>Location</p>
-                            <TextField size="small" sx={{ width: '80%', height: 35, '& .MuiInputBase-root': { height: 35 }, '& .MuiFilledInput-input': { fontSize: 14 } }} id="filled-basic" label="" variant="filled" />
+                            <TextField id="outlined-multiline-flexible" fullWidth size="small" sx={{ '& .MuiFilledInput-input': { fontSize: 13 } }} label="" placeholder='Location' value={location} onChange={(e) => setLocation(e.target.value)} multiline maxRows={3} />
                         </div>
                         <div className="mb-4 flex items-center gap-2 w-full">
-                            <p className='font-bold min-w-[70px]'>Description</p>
-                            <TextField id="outlined-multiline-flexible" size="small" sx={{ width: '80%' }} label="" maxRows={5} />
-                            {/* Figure out whats wrong with the multiline text field */}
+                            <TextField id="outlined-multiline-flexible" fullWidth size="small" sx={{ '& .MuiFilledInput-input': { fontSize: 13 } }} label="" placeholder='Description' value={description} onChange={(e) => setDescription(e.target.value)} multiline maxRows={3} />
                         </div>
-                        <div className="mb-8 mt-5 flex justify-center gap-2 w-full">
+                        <div className="mb-8 mt-8 flex justify-center gap-4 w-full">
                             <button onClick={handleAddEvent} className="text-black text-2xl font-normal font-['VT323'] outline outline-2 px-4 py-2 rounded" style={{ outlineColor: '#07BCFA' }}>
                                 Add to Calendar
                             </button>
+                            <button onMouseEnter={() => setTrash(true)} onMouseLeave={() => setTrash(false)} onClick={() => { setEventTitle(""); setStart(""); setEnd(""); setLocation(""); setDescription(""); }} className="bg-transparent hover:bg-gray-200 transition-colors duration-200 flex items-center justify-center p-2 rounded-full">
+                                <img src={trash ? "/open_trash.png" : "/closed_trash.png"} alt="Trash" className="w-9 h-9" />
+                            </button>     
                         </div>
                     </div>
                 )}
